f4f350f92c515571fada54cfc96fb0c2a2114bd9
Kartik2negi <kartik2negi@gmail.com>
2025-09-19 00:16:02 +0530

    feat: support custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,10 +1,24 @@
-function add(input) {
+function escapeRegExp(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
 
+function add(input) {
     if (input === "") return 0;
 
-    // split by comma OR newline
-    const parts = input.split(/,|\n/);
-    return parts.reduce((sum, n) => sum + Number(n), 0);
+    let numbersPart = input;
+    let separator = /,|\n/;
 
+    // custom delimiter support
+    if (input.startsWith('//')) {
+        const [delim, rest] = input.slice(2).split('\n');
+        separator = new RegExp(escapeRegExp(delim));
+        numbersPart = rest;
+    }
+
+    const nums = numbersPart.split(separator).map(n => Number(n.trim()));
+    const negatives = nums.filter(n => n < 0);
+
+    return nums.reduce((sum, n) => sum + n, 0);
 }
+
 module.exports = { add };
