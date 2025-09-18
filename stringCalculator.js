1db1f91e1d7b61ad0ce57815214cffb46d1ccaa6
Kartik2negi <kartik2negi@gmail.com>
2025-09-19 00:03:26 +0530

    feat: support comma as separator

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,10 @@
 function add(input) {
 
     if (input === "") return 0;
-    return Number(input);
+
+    // split by comma and convert each piece to number
+    const parts = input.split(",");
+    return parts.reduce((sum, n) => sum + Number(n), 0);
 
 }
 module.exports = { add };
