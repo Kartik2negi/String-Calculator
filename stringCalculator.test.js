488aaa1917d24741da74223cb2fe79378037638e
Kartik2negi <kartik2negi@gmail.com>
2025-09-18 23:55:45 +0530

    test: add single string case

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,4 +5,9 @@ describe('String Calculator', () => {
   it('returns 0 for empty string', () => {
     expect(add('')).toBe(0);
   });
+
+  it('returns the number for a single number', () => {
+    expect(add('1')).toBe(1);
+    expect(add('42')).toBe(42);
+  });
 });
